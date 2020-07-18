b6baef92fb7d27d5401c06d7a37cc5e63ac81b3c
KariukiAbel <abelmuriithi@gmail.com>
2020-07-19 01:53:38 +0300

    used flex to move the panels

--- src/Container.js
+++ src/Container.js
@@ -2,8 +2,10 @@ import React, { PureComponent } from 'react';
 import LeftPanel from './LeftPanel';
 import RightPanel from './RightPanel';
 
+import './App.css';
+
 export default class Container extends PureComponent {
     render() {
-        return <div > < p > Chat app < /p ><div><LeftPanel/ > < RightPanel / > < /div > < /div > ;
+        return <div > < p > Chat app < /p ><div className="wrapper"><LeftPanel/ > < RightPanel / > < /div > < /div > ;
     }
 }
